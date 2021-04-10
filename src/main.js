c6b2dffa0193fe8ae28b4ee6457cea579fd7673b
Suzan1205 <shz826@126.com>
2021-04-10 15:33:03 +0800

    第一次提交

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+// import router from './router'
+// import store from './store'
+// import VueLazyLoad from 'vue-lazyload'
+
 
 Vue.config.productionTip = false
 
